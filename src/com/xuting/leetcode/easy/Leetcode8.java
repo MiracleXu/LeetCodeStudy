25414b1524046f2959d6d1e3b79c0afb6f3344e0
xuting <xuting@lvmama.com>
2019-07-22 17:16:01 +0800

    LeetCode

--- src/com/xuting/leetcode/easy/Leetcode8.java
+++ src/com/xuting/leetcode/easy/Leetcode8.java
@@ -34,11 +34,14 @@ public class Leetcode8 {
         for (int i = 0;i<nums.length;i++){
             System.out.println(nums[i]);
         }
+
+
     }
 
     public static void main(String[] args){
         int[] nums = {0,0,1,0,1,2,0,0,2,0};
 //        int[] nums = {0,1,0,3,12};
         moveZeroes(nums);
+        System.out.println(Integer.toBinaryString(16));
     }
 }
